85639130aaf53ab876932367fa6d76e4acb247c3
Daniil Borisevich <dborisevich@whoosh.bike>
2026-03-19 17:28:59 +0300

    added 35

--- web/src/test/java/WebTest.java
+++ web/src/test/java/WebTest.java
@@ -1,9 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class WebTest {
 
+  @DisplayName("Веб калькулятор боооньк")
   @Test
   public void webTest() {
     assertEquals(2, 1 + 1);
