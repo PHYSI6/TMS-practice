b18741e47e4b7d3208b3cd9260eaac257cc41f4e
Daniil Borisevich <dborisevich@whoosh.bike>
2026-03-19 17:28:37 +0300

    added 35

---
+++ web/src/test/java/WebTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class WebTest {
+
+  @Test
+  public void webTest() {
+    assertEquals(2, 1 + 1);
+  }
+}
